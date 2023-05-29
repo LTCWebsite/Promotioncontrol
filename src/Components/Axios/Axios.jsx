04106b2ddca5cf7f6b5e056afed00479ad17244b
Phonema2 <phonma.ktvs@gmail.com>
2023-05-29 14:54:27 +0700

    commit

--- src/Components/Axios/Axios.jsx
+++ src/Components/Axios/Axios.jsx
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const Axios = axios.create({
-    baseURL: "http://172.28.26.146:7070",
-})
-export default Axios
+  baseURL: "http://172.28.26.146:7070",
+  // baseURL: "http://172.28.26.146:1715/",
+});
+export default Axios;
